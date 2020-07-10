4ce7b09055a252ca7d53977c737a6a1ff5997196
Joey Yang <joeyyang.ai@gmail.com>
2020-07-09 23:29:37 -0400

    boilerplate for driver

---
+++ include/bno055_driver.h
@@ -0,0 +1,19 @@
+#ifndef BNO055_DRIVER_H
+#define BNO055_DRIVER_H
+
+namespace bno055_imu
+{
+class IMU
+{
+
+public:
+  IMU();
+  
+  ~IMU();
+
+private:
+
+};
+}
+
+#endif // BNO055_DRIVER_H
---
+++ src/bno055_driver.cpp
@@ -0,0 +1,1 @@
+#include "bno055_driver.h"
