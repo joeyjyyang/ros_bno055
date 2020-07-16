afbab16652fe60ba070a03d74797767798c3dabb
Joey Yang <joeyyang.ai@gmail.com>
2020-07-16 01:04:34 -0400

    defined constants for modes

--- include/bno055_driver.h
+++ include/bno055_driver.h
@@ -1,6 +1,26 @@
 #ifndef BNO055_DRIVER_H
 #define BNO055_DRIVER_H
 
+// [PWR_MODE]
+enum class PowerMode : unsigned char
+{
+  normalMode = 0x00,
+  lowPowerMode = 0x01,
+  suspendMode = 0x02
+};
+
+// [OPR_MODE] 
+enum class OperationMode : unsigned char
+{
+  configMode = 0x00,
+  accOnly = 0x01,
+  magOnly = 0x02,
+  gyroOnly = 0x03,
+  imu = 0x08,
+  compass = 0x09, // Requires calibration
+  ndof = 0x0C// FMC is turned ON
+};
+
 namespace bno055_imu
 {
 class IMU
