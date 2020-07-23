4eda51becd1f69359cca434a22ec7d46f5f9565d
Joey Yang <joeyyang.ai@gmail.com>
2020-07-23 02:59:39 +0000

    adding missing op mode constants

--- include/bno055_driver.h
+++ include/bno055_driver.h
@@ -13,12 +13,20 @@ enum class PowerMode : unsigned char
 enum class OperationMode : unsigned char
 {
   configMode = 0x00,
+  /* Non-Fusion Modes */
   accOnly = 0x01,
   magOnly = 0x02,
   gyroOnly = 0x03,
+  accMag = 0x04,
+  accGyro = 0x05,
+  magGyro = 0x06,
+  amg = 0x07,
+  /* Fusion Modes */
   imu = 0x08,
   compass = 0x09, // Requires calibration
-  ndof = 0x0C// FMC is turned ON
+  m4g = 0x0A,
+  nDofFmcOff = 0x0B,
+  nDof = 0x0C// FMC is turned ON
 };
 
 namespace bno055_imu
