d94f5a6abd99344cd760d14d86b4c0093ae0b202
Joey Yang <joeyyang.ai@gmail.com>
2020-08-21 20:53:53 -0400

    Accessing i2c bus and bno055 i2c address

--- src/bno055_driver.cpp
+++ src/bno055_driver.cpp
@@ -7,29 +7,21 @@ Imu::Imu()
   cout << "BNO055 Imu initialized." << endl;
 }
 
-int Imu::accessI2c(string i2c_bus, int i2c_address, unsigned char chip_address)
+int Imu::accessI2c()
 {
   int file_desc;
-  char buffer[5];
-
-  if (file_desc = open(i2c_bus, O_RDWR) < 0)
+  if ((file_desc = open(I2C_BUS, O_RDWR)) < 0)
   {
-    cout << "ERROR: Could not open I2C bus: " << i2c_bus << endl;    
-    exit(1);
-  } 
-  
-  if (ioctl(file_desc, I2C_SLAVE, i2c_address) < 0) 
-  {
-    cout << "ERROR: Could not locate BNO055 Imu at address: " << i2c_address << endl;
+    std::cout << "ERROR: Could not open I2C bus: " << I2C_BUS << std::endl; 
+    perror("ERROR: ");
     exit(1);
   }
   
-  buffer[0] = chip_address;
-  buffer[1] = 0x11; // Test value.
-
-  if (write(file_desc, buffer, 2) != 2) 
+  if (ioctl(file_desc, I2C_SLAVE, I2C_ADDRESS) < 0)
   {
-    cout << "ERROR: Could not write to register: " << chip_address << endl;
+    std::cout << "ERROR: Could not locate BNO055 Sensor at address: " << I2C_ADDRESS << std::endl;
+    perror("ERROR: ");
+    exit(1);
   }
 }
 
