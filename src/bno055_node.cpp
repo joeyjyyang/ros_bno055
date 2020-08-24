f134e7e8e73c9c77b4002377bc6fc5af670d02cd
Joey Yang <joeyyang.ai@gmail.com>
2020-08-24 00:49:51 -0400

    Add ROS node boilerplate

--- src/bno055_node.cpp
+++ src/bno055_node.cpp
@@ -2,10 +2,24 @@
 
 #include <ros/ros.h>
 
+namespace bno055_driver
+{
+class Bno055Node
+{
+public:
+	Bno055Node(const ros::NodeHandle& nh) : nh_(nh)
+  {}
+	~Bno055Node();
+private:
+	ros::NodeHandle nh_;
+};
+} // namespace bno055_driver
+
 int main(int argc, char* argv[])
 {
   ros::init(argc, argv, "bno055_node");
   ros::NodeHandle nh("~");
+  bno055_driver::Bno055Node bno055_node(nh);
   ros::spin();
 
   return 0;
