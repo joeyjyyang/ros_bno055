e1d7ddf0416d185150d6000d63ce3bc347172fd0
Joey Yang <yangj30@mcmaster.ca>
2020-11-06 10:29:09 -0500

    call getCalibRadius in ROS node

--- src/bno055_node.cpp
+++ src/bno055_node.cpp
@@ -41,6 +41,10 @@ public:
     {
       ROS_ERROR("Failed to get calibration offset data.");
     }
+    if (bno055_driver_.getCalibRadius() < 0)
+    {
+      ROS_ERROR("Failed to get calibration radius data.");
+    }
     
     imu_pub_ = nh_.advertise<sensor_msgs::Imu>("imu", 1);
     euler_pub_ = nh_.advertise<ros_bno055::OrientationEuler>("orientation_euler", 1);
